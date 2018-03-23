d16d46b63c69cc759c151c7ae1464812b8ce8da2
kaybutswag <kbwagner426@gmail.com>
2018-03-23 09:12:29 -0500

    try use mongo client

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI,{
+  useMongoClient: true
+});
 
 app.get("/clearall", function(req, res) {
     db.Article.remove(
